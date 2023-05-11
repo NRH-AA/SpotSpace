3023ec7fb77c628745de1dd79141531a71827a71
NRH-AA <nheinz1995@live.com>
2023-05-10 19:59:34 -0700

    Fix update spot modal

--- frontend/src/components/UpdateSpotModal/index.js
+++ frontend/src/components/UpdateSpotModal/index.js
@@ -25,15 +25,14 @@ const UpdateSpotModal = () => {
     
     if (!spot) spot = spotStateSingle;
     
-    console.log(spot);
-    
-    const [country, setCountry] = useState(spot.country ? spot.country : '');
-    const [address, setAddress] = useState(spot.address ? spot.address : '');
-    const [city, setCity] = useState(spot.city ? spot.city : '');
-    const [state, setState] = useState(spot.state ? spot.state : '');
-    const [description, setDescription] = useState(spot.description ? spot.description : '');
-    const [name, setName] = useState(spot.name ? spot.name : '');
-    const [price, setPrice] = useState(spot.price ? spot.price : '');
+    const [country, setCountry] = useState(spot?.country || '');
+    const [address, setAddress] = useState(spot?.address || '');
+    const [city, setCity] = useState(spot?.city || '');
+    const [state, setState] = useState(spot?.state || '');
+    const [zipcode, setZipcode] = useState(spot?.zipcode || '')
+    const [description, setDescription] = useState(spot?.description || '');
+    const [name, setName] = useState(spot?.name || '');
+    const [price, setPrice] = useState(spot?.price || '');
     const [errors, setErrors] = useState([]);
     const [formErrors, setFormErrors] = useState({});
     
@@ -44,22 +43,18 @@ const UpdateSpotModal = () => {
         const err = {};
         
         if (!country) err.country = "Country is required";
-        if (country.length < 5 || country.length > 20) err.country = "Country length must be 5-20 characters";
         if (!address) err.address = "Address is required";
-        if (address.length < 5 || address.length > 20) err.address = "Address length must be 5-20 characters";
         if (!city) err.city = "City is required"
-        if (city.length < 5 || city.length > 15) err.city = "City length must be 5-15 characters";
         if (!state) err.state = "State is required";
-        if (state.length < 5 || state.length > 15) err.state = "State length must be 5-15 characters";
-        if (description.length < 30) err.desc = "Description needs a minimum of 30 characters";
-        if (description.length > 250) err.desc = "Description needs a maximum of 250 characters";
-        if (!name) err.name = "Name is required";
-        if (name.length < 5 || name.length > 20) err.name = "Title length must be 5-20 characters";
-        if (!price) err.price = "Price is required";
-        if (price === 0) err.price = "Price cannot be zero" 
-        if (description.length < 30) err.desc = "Description needs a minimum of 30 characters";
         if (!name) err.name = "Name is required";
         if (!price) err.price = "Price is required";
+        
+        if (address.length < 5 || address.length > 30) err.address = "Address (5-30) Characters";
+        if (city.length < 5 || city.length > 20) err.city = "City (5-20) Characters";
+        if (description.length < 10 || description.length > 250) err.desc = "Description (10-250) Characters";
+        if (name.length < 5 || name.length > 20) err.name = "Title (5-20) Characters";
+        
+        if (price === 0) err.price = "Price cannot be zero";
         
         return err;
     }
@@ -99,7 +94,7 @@ const UpdateSpotModal = () => {
     };
     
     return (
-        <div className="create-spot-wrapper">
+        <div id="update-spot-wrapper">
             <h2>Update your Spot</h2>
             
             <div>
@@ -109,69 +104,83 @@ const UpdateSpotModal = () => {
             </div>
             
             <form onSubmit={handleSubmit}>
-                <div id="create-spot-top">
-                    <div className="create-spot-input-div">
-                        <label>Country <span className="error-msg">{formErrors.country ? formErrors.country : ''}</span></label>
-                        <input className="create-spot-input" type="text" placeholder='Country' value={country}
-                            onChange={(e) => setCountry(e.target.value)}
-                        ></input>
-                    </div>
-                    
-                    <div className="create-spot-input-div">
+                <div id="update-spot-top">
+                    <div className="update-spot-input-div">
                         <label>Street Address <span className="error-msg">{formErrors.address ? formErrors.address : ''}</span></label>
-                        <input className="create-spot-input" type="text" placeholder='Address' value={address}
+                        <input className="main-input-style update-spot-input" type="text" placeholder='Address' value={address}
                             onChange={(e) => setAddress(e.target.value)}
                         ></input>
                     </div>
                     
-                    <div id="city-state-div">
-                        <div id="create-spot-input-city-div">
-                            <label>City <span className="error-msg">{formErrors.city ? formErrors.city : ''}</span></label>
-                            <input className="create-spot-input" type="text" placeholder='City' value={city}
-                                onChange={(e) => setCity(e.target.value)}
-                            ></input>
-                        </div>
+                    <div className="update-spot-input-div">
+                        <label>Country <span className="error-msg">{formErrors.country ? formErrors.country : ''}</span></label>
+                        <input className="main-input-style update-spot-input" type="text" placeholder='Country' value={country}
+                            onChange={(e) => setCountry(e.target.value)}
+                        ></input>
+                    </div>
+                    
+                    <div className="update-spot-input-div"> 
                         <div id="create-spot-input-state-div">
-                            <label>State <span className="error-msg">{formErrors.state ? formErrors.state : ''}</span></label>
-                            <input className="create-spot-input" type="text" placeholder='State' value={state}
+                            <label>State <span className="error-msg">{formErrors.state ? formErrors.state : ''}</span></label><br></br>
+                            <input className="main-input-style update-spot-input" type="text" placeholder='State' value={state}
                                 onChange={(e) => setState(e.target.value)}
                             ></input>
                         </div>
                     </div>
+                    
+                    <div className="update-spot-input-div">
+                        <div id="create-spot-input-city-div">
+                            <label>City <span className="error-msg">{formErrors.city ? formErrors.city : ''}</span></label><br></br>
+                            <input className="main-input-style update-spot-input" type="text" placeholder='City' value={city}
+                                onChange={(e) => setCity(e.target.value)}
+                            ></input>
+                        </div>
+                    </div>
+                    
+                    <div className="update-spot-input-div">
+                        <label>Zipcode <span className="main-error-li">{formErrors.zipcode ? formErrors.zipcode : ''}</span></label>
+                        <input className="main-input-style update-spot-input" type="text" 
+                            placeholder='Zipcode' 
+                            value={zipcode}
+                            onChange={(e) => setZipcode(e.target.value)}
+                        ></input>
+                    </div>
+                    
+                    
                 </div>
                 
                 <div>
                     <h3>Description</h3>
                     
-                    <div id="desc-div">
-                        <textarea id="desc-textarea" name="description" value={description}
+                    <span className="main-error-li">{formErrors.desc ? formErrors.desc : ''}</span>
+                    <div id="update-desc-div">
+                        <textarea id="update-desc-textarea" name="description" value={description}
                             placeholder='Please write at least 30 characters'
                             onChange={(e) => setDescription(e.target.value)}
                         > 
                         </textarea>
-                        <span className="error-msg">{formErrors.desc ? formErrors.desc : ''}</span>
                     </div>
                 </div>
                 
-                <div id="title-div">
+                <div id="update-title-div">
                     <h3>Title</h3>
                     
                     <div>
-                        <input className="create-spot-input create-spot-input2" type='text' value={name}
+                        <span className="main-error-li">{formErrors.name ? formErrors.name : ''}</span>
+                        <input className="main-input-style update-spot-input" type='text' value={name}
                             placeholder='Name of your spot'
                             onChange={(e) => setName(e.target.value)}
                         ></input>
-                        <span className="error-msg">{formErrors.name ? formErrors.name : ''}</span>
                     </div>
                 </div>
                 
                 <div id="price-div">
                     <h3 id="price-h3">Price per night</h3>
-                    <span>$ </span><input className="create-spot-input" id="price-input" type="text" value={price ? price : 0}
+                    <span className="main-error-li">{formErrors.price ? formErrors.price : ''}</span>
+                    <span>$ </span><input className="main-input-style update-spot-input" id="price-input" type="text" value={price ? price : 0}
                         placeholder='price'
                         onChange={(e) => setPrice(e.target.value)}
-                        ></input>
-                    <span className="error-msg">{formErrors.price ? formErrors.price : ''}</span>
+                    ></input>
                 </div>
                 
                 <div id="update-spot-button-div">

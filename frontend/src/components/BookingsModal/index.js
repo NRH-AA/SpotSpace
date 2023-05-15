9680561456ed3cbab5d732c02ff57f9d99dc0b7b
NRH-AA <nheinz1995@live.com>
2023-05-15 05:07:13 -0700

    Booking calander styling and handle disabled dates

--- frontend/src/components/BookingsModal/index.js
+++ frontend/src/components/BookingsModal/index.js
@@ -3,19 +3,36 @@ import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { useModal } from "../../context/Modal";
 import Calendar from 'react-calendar';
+import { differenceInCalendarDays } from 'date-fns';
 import './Bookings.css';
 
 const BookingsModal = ({spotId}) => {
     const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date());
     
     
     
     if (!spotId) return null;
     
+    function isSameDay(a, b) {
+        return differenceInCalendarDays(a, b) === 0;
+    }
+    
+    const tileDisabled = ({date, view}) => {
+        const disabledDates = [];
+        return disabledDates.find(dDate => isSameDay(dDate, date));
+    }
+    
+    
     return <div id='bookings-modal-container'>
-        <Calendar onChange={setStartDate} value={startDate}></Calendar>
-        <Calendar onChange={setEndDate} value={endDate}></Calendar>
+        <Calendar 
+            onChange={setStartDate} 
+            value={startDate}
+            minDate={new Date()}
+            minDetail='decade'
+            calendarType='US'
+            selectRange
+            tileDisabled={tileDisabled}
+        ></Calendar>
     </div>
 };
 

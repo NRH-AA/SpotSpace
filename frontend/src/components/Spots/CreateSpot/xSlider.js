3a8ac87797772c52fba992fdea9e4a73e3060aa6
NRH-AA <nheinz1995@live.com>
2023-06-07 22:11:50 -0700

    Create Spot Nights Slider

---
+++ frontend/src/components/Spots/CreateSpot/xSlider.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import Slider from 'react-input-slider';
+
+const CreateSpotNightSlider = () => {
+    const [nights, setNights] = useState(1);
+    const defaultAmount = 124;
+    
+    return (<>
+        <p style={{fontSize: "34px", marginBottom: "0px", color: "#00e200"}}>SpotSpace it.</p>
+        <p style={{fontSize: "38px", marginTop: "5px", marginBottom: "0px"}}>You could earn</p>
+        <p style={{fontSize: "65px", marginTop: "20px", marginBottom: "0px"}}>${defaultAmount * nights}</p>
+        <p style={{paddingBottom: "25px", marginTop: "20px", fontWeight: "500"}}><b><u>{nights === 1 ? '1 night' : `${nights} nights`}</u></b> at an estimated {defaultAmount} a night</p>
+        <Slider
+            asix="x"
+            x={nights}
+            xmin={1}
+            xmax={30}
+            onChange={({x}) => setNights(x)}
+                    
+            styles={{
+                track: {
+                    backgroundColor: '#c5c5c5',
+                    width: "95%",
+                    height: "4px"
+                },
+                active: {
+                    backgroundColor: '#242424'
+                },
+                thumb: {
+                    width: 25,
+                    height: 25,
+                    backgroundColor: "black"
+                },
+                disabled: {
+                    opacity: 0.5
+                }
+            }}
+        />
+    </>)
+};
+
+export default CreateSpotNightSlider;
